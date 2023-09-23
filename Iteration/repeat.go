bd4abef774a663250455d48d9683cc3a531e1fc5
Karolayne <karolayneamabile@gmail.com>
2023-09-23 19:55:38 -0300

    saturday

--- Iteration/repeat.go
+++ Iteration/repeat.go
@@ -5,7 +5,7 @@ const repeatCount = 5
 func Repeat(character string) string { // declare a function named repeat that takes an input argument character of type string and returns a string as the result
 	var repeated string // declare a variable/name/data type
 	for i := 0; i < repeatCount; i++ {
-		repeated += character
+		repeated += character // the add and assignment operator
 	}
 	return repeated
 }
